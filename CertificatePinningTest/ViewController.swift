c008dfefc4521407195b8db810397822b6b91077
Konstantin Braun <konstantin.braun@netcetera.ch>
2023-07-13 10:59:28 +0200

    Add webview with certificate pinning

--- CertificatePinningTest/ViewController.swift
+++ CertificatePinningTest/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView.navigationDelegate = self
+        
+        let urlRequest = URLRequest(url: URL(string: "https://www.googletagmanager.com")!)
+        
+        webView.load(urlRequest)
     }
+}
 
-
+extension ViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        didReceive challenge: URLAuthenticationChallenge,
+        completionHandler: @escaping (URLSession.AuthChallengeDisposition,
+                                      URLCredential?) -> Void
+    ) {
+        guard challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
+              let serverTrust = challenge.protectionSpace.serverTrust else {
+            completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
+            return
+        }
+        
+        var secresult: CFError?
+        let status = SecTrustEvaluateWithError(serverTrust, &secresult)
+        
+        guard status,
+           let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
+            completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
+            return
+        }
+        
+        let serverCertificateData = SecCertificateCopyData(serverCertificate)
+        let data = CFDataGetBytePtr(serverCertificateData);
+        let size = CFDataGetLength(serverCertificateData);
+        let cert1 = NSData(bytes: data, length: size)
+        let file_der = Bundle.main.path(forResource: "*.google-analytics.com", ofType: "cer")
+        
+        if let file = file_der,
+           let cert2 = NSData(contentsOfFile: file),
+           cert1.isEqual(to: cert2 as Data) {
+            completionHandler(URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust:serverTrust))
+            return
+        }
+        
+        completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
+    }
 }
 
