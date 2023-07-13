697cb7ec348781d9ddc20958d477b434713ff006
Konstantin Braun <konstantin.braun@netcetera.ch>
2023-07-13 09:18:04 +0200

    Initial Commit

---
+++ CertificatePinningTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CertificatePinningTest
+//
+//  Created by Konstantin Braun on 13.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
